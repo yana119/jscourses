28d02cf1430a79850f3e03a42a844a34f9ee3d72
Yana Kumanina <yana119@bk.ru>
2019-07-10 12:19:38 +0400

    second homework!

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('Hello, Mediasoft!');
+let a = prompt('¬ведите первое число:');
+let b = prompt('¬ведите второе число:');
+let operator = prompt('¬ведите знак');
+
+ if (operator == '+') {
+     prompt(+a + +b);
+}   else if (operator == '-') {
+     alert(+a - +b);
+}   else if (operator == '*') {
+     alert(+a * +b);
+}   else if (operator == '/') {
+     alert(+a / +b);
+}   else{
+     alert('¬ведите нужную операцию!');
+}
